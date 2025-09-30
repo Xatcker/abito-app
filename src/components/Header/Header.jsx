0afc27b144bc677ac1d30da8370afe4776db3f44
Georgy Makarov <mutant20012001@mail.ru>
2025-09-30 19:17:34 +0300

    added a site page

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import './Header.css'
+
+export const Header = ()=>{
+    return (
+        <div>
+            <header className="header">
+        <div className="container">
+            <div className="header-box">
+                <a href="./index.html" className="header-logo">
+                    <img src="/images/logo.svg" alt="logo" />
+                    <span>Abito</span>
+                </a>
+                <div className="header-controls">
+                    <button className="btn btn-outline">Вход и регистрация</button>
+                    <button className="btn btn-primary">Подать объявление</button>
+                </div>
+
+                <div className="header-burger">
+                    <img src="/images/burger.svg" alt="burger" />
+                </div>
+
+            </div>
+        </div>
+    </header>
+        </div>
+    )
+}
---
+++ src/constants.js
@@ -0,0 +1,63 @@
+
+export const cardArray = [
+    {
+        id: 0,
+        title: 'Пвх один',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    },
+    {
+        id: 1,
+        title: 'Пвх три',
+        price: '171 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    },
+    {
+        id: 2,
+        title: 'Пвх сыр',
+        price: '172 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    }
+    ,
+    {
+        id: 3,
+        title: 'Пвх 4 материал 2й сорт',
+        price: '173 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    }
+    ,
+    {
+        id: 4,
+        title: 'Пвх 5 материал 2й сорт',
+        price: '174 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    }
+    ,
+    {
+        id: 5,
+        title: 'Пвх 6 материал 2й сорт',
+        price: '175 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    }
+    ,
+    {
+        id: 6,
+        title: 'Пвх материал 1й сорт',
+        price: '176 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    }
+];
